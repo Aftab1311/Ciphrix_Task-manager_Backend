bd1fece4ab7c801b5d8bc5f6e580a9ffea117e07
aftab1311 <md.aftabehsan@gmail.com>
2025-11-29 16:13:19 +0530

    Cors added

--- src/server.js
+++ src/server.js
@@ -9,8 +9,24 @@ const taskRoutes = require('./routes/tasks');
 const app = express();
 connectDB();
 
-app.use(cors());
+const allowedOrigins = [
+  "https://ciphrix-task-manager-frontend.vercel.app",
+  "http://localhost:5173"
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+app.options("*", cors()); // Important for preflight
+
 app.use(express.json());
+
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 
