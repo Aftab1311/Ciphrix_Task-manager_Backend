5eb12cf4f864be192dcce06b5c1ff08efdd3ff71
aftab1311 <md.aftabehsan@gmail.com>
2025-11-29 19:02:25 +0530

    minor chnages

--- server.js
+++ server.js
@@ -2,12 +2,29 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./src/config/db');
+const app = express();
 
 const authRoutes = require('./src/routes/auth');
 const taskRoutes = require('./src/routes/tasks');
 
-const app = express();
-connectDB();
+let isconnected = false;
+
+async function initializeDBConnection() {
+  try {
+    await connectDB();
+    isconnected = true;
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error("Database connection failed:", error);
+  }
+}
+
+app.use(async (req, res, next) => {
+  if (!isconnected) {
+    await initializeDBConnection();
+  }
+  next();
+});
 
 const allowedOrigins = [
   "https://ciphrix-task-manager-frontend.vercel.app",
@@ -38,5 +55,7 @@ app.use((req, res) => {
   res.status(404).json({ error: "Route not found" });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
+module.exports = app;
